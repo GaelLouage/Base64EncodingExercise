f7ce7f4552e485e8effe69d43cf9c1d3bef89636
Gaël Louage <gaellouage@hotmail.com>
2025-06-19 15:13:13 +0200

    base64 encoding / decoding exercise

---
+++ Base64EncodingEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Buffers.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(ToBase64("this is a string!!"));
+        Console.WriteLine(FromBase64(ToBase64("this is a string!!")));
+    }
+    public static string ToBase64(string s)
+    {
+        return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(s));
+    }
+
+    public static string FromBase64(string s)
+    {
+
+        var base64EncodedBytes = System.Convert.FromBase64String(s);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+}
